5c6bef4f58a6606636c3d03decab512d1fa5e172
Shardly Romelus <rshardlypicard89@gmail.com>
2021-05-10 21:49:38 -0400

    secure user password with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,30 @@
 class User < ApplicationRecord
+    include BCrypt
+
     validates :email, presence: true, uniqueness: true
     validates :first_name, :last_name, presence: true
-    validates :password, presence: true, length: { in: 6..10 }
+    validates :password_hash, presence: true, length: { minimum: 6 }
     validate :confirm_pass
 
     has_many :notes, dependent: :destroy
 
     scope :login, -> (email) { where("email= ?", email)}
 
+    def password
+        @password ||= Password.new(password_hash)
+    end
+      
+    def password=(new_password)
+        @password = Password.create(new_password)
+        self.password_hash = @password
+    end
 
     private
     def confirm_pass
-        if self.password != self.confirm_password
+        if BCrypt::Password.new(self.password_hash) != self.confirm_password
             errors.add(:confirm_password, 'does not match')
+        else
+            self.confirm_password = ''
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  # root 'users#index'
+  root 'sessions#login'
+
+  get '/sessions/login', to: 'sessions#login', as: :login
+  get '/sessions/signup', to: 'sessions#signup', as: :signup
+  get '/sessions/new_session', to: 'sessions#login'
+  get '/sessions/registration', to: 'sessions#signup'
+  post '/sessions/new_session', to: 'sessions#new_session', as: :new_session
+  post '/sessions/registration', to: 'sessions#registration', as: :registration
+  delete '/sessions/signout', to: 'sessions#destroy_session', as: :signout
 
   resources :notes do
     resources :users
   end
 
-  get 'login', to: "users#login"
+  # get 'login', to: "users#login"
+  # get 'signup', to: "users#signup"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
